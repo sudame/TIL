1e8b895bfcac1f4b6ea0d9d333b299a664f8389d
sudame <su.glasses.923@gmail.com>
2019-01-07 00:00:45 +0900

    add: hello page

--- rails-tutorial-sample-app/config/routes.rb
+++ rails-tutorial-sample-app/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  root "application#hello"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
