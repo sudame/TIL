34b1be1e0d36557f72b2b8c468cfdb0ee4112e18
sudame <su.glasses.923@gmail.com>
2019-01-08 00:01:12 +0900

    add: create action

--- rails-tutorial-sample-app/app/controllers/sessions_controller.rb
+++ rails-tutorial-sample-app/app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 end
