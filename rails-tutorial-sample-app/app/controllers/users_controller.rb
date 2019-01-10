1933453bbed0d789c3c953e445534a2b910f306e
sudame <su.glasses.923@gmail.com>
2019-01-11 00:42:04 +0900

    add: only admin can delete users

--- rails-tutorial-sample-app/app/controllers/users_controller.rb
+++ rails-tutorial-sample-app/app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
-  def  index
+  def index
     @users = User.paginate(page: params[:page])
   end
 
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User deleted.'
+    redirect_to user_url
+  end
+
   private
 
   def user_params
@@ -54,5 +61,9 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
