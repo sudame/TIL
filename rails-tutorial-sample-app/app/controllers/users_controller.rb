ca8c03dd9599d48ed64df8355b91d78361b62435
sudame <su.glasses.923@gmail.com>
2019-01-10 22:28:35 +0900

    add: user edit view test

--- rails-tutorial-sample-app/app/controllers/users_controller.rb
+++ rails-tutorial-sample-app/app/controllers/users_controller.rb
@@ -22,6 +22,14 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'users/edit'
+    end
+  end
+
   private
 
   def user_params
