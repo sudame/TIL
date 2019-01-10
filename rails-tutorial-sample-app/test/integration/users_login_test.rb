1239710bbc0be45d44be2c148ca1ff6f3f2dbd28
sudame <su.glasses.923@gmail.com>
2019-01-10 14:37:29 +0900

    fix: 2 minor bug

--- rails-tutorial-sample-app/test/integration/users_login_test.rb
+++ rails-tutorial-sample-app/test/integration/users_login_test.rb
@@ -27,9 +27,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 end
