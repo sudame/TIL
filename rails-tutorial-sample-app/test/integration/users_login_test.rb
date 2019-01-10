739a415ee8bdc79f2e419b1b6ea4ad22fece425a
sudame <su.glasses.923@gmail.com>
2019-01-10 15:23:24 +0900

    add: remember test

--- rails-tutorial-sample-app/test/integration/users_login_test.rb
+++ rails-tutorial-sample-app/test/integration/users_login_test.rb
@@ -37,4 +37,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?('')
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
