972354babe55aeb9116827325976ceffc75b79db
sudame <su.glasses.923@gmail.com>
2019-01-10 22:39:30 +0900

    add: can update user profile

--- rails-tutorial-sample-app/app/controllers/users_controller.rb
+++ rails-tutorial-sample-app/app/controllers/users_controller.rb
@@ -25,6 +25,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
     else
       render 'users/edit'
     end
