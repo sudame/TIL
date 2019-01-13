af096cc8c7b5c57e57d09df0390a39413b98fd05
sudame <su.glasses.923@gmail.com>
2019-01-13 22:55:14 +0900

    add: expired token test

--- rails-tutorial-sample-app/test/integration/password_resets_test.rb
+++ rails-tutorial-sample-app/test/integration/password_resets_test.rb
@@ -40,4 +40,16 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_redirected_to user
   end
 
+  test 'expired token' do
+    get new_password_reset_path
+    post password_resets_path, params: {password_reset: {email: @user.email}}
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token), params: {email: @user.email, user: {password: 'foobaz',
+                                                                                      password_confirmation: 'foobaz'}}
+    assert_response :redirect
+    follow_redirect!
+    assert_match /expired/i, response.body
+  end
+
 end
