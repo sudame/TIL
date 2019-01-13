46051d06710ae347939af15f673d9f047e3881a7
sudame <su.glasses.923@gmail.com>
2019-01-13 22:37:27 +0900

    fix: if else block

--- rails-tutorial-sample-app/app/controllers/password_resets_controller.rb
+++ rails-tutorial-sample-app/app/controllers/password_resets_controller.rb
@@ -13,9 +13,10 @@ class PasswordResetsController < ApplicationController
       @user.send_password_reset_email
       flash[:info] = 'Email sent with password reset instructions'
       redirect_to root_url
+    else
+      flash.now[:danger] = 'Email address not found'
+      render 'new'
     end
-    flash.now[:danger] = 'Email address not found'
-    render 'new'
   end
 
   def update
