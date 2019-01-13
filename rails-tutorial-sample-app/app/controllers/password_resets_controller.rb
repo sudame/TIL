a81a45615f582e456f10947c5b7aa4bbc7e9d95e
sudame <su.glasses.923@gmail.com>
2019-01-13 22:44:48 +0900

    fix: typo

--- rails-tutorial-sample-app/app/controllers/password_resets_controller.rb
+++ rails-tutorial-sample-app/app/controllers/password_resets_controller.rb
@@ -38,7 +38,7 @@ class PasswordResetsController < ApplicationController
   private
 
   def user_params
-    params.requre(:user).permit(:password, :password_confirmation)
+    params.require(:user).permit(:password, :password_confirmation)
   end
 
   def get_user
