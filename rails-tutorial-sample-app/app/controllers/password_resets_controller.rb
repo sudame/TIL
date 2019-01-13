f38ceb20cd75392ce1fe5acdd4eed5c1a259016e
sudame <su.glasses.923@gmail.com>
2019-01-13 22:28:24 +0900

    add: password reset controllers

--- rails-tutorial-sample-app/app/controllers/password_resets_controller.rb
+++ rails-tutorial-sample-app/app/controllers/password_resets_controller.rb
@@ -1,7 +1,8 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
-  
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -18,12 +19,26 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = 'Password has been reset'
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def edit
   end
 
   private
+
+  def user_params
+    params.requre(:user).permit(:password, :password_confirmation)
+  end
 
   def get_user
     @user = User.find_by(email: params[:email])
@@ -32,6 +47,13 @@ class PasswordResetsController < ApplicationController
   def valid_user
     unless @user&.activated? && @user.authenticated?(:reset, params[:id])
       redirect_to root_url
+    end
+  end
+
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = 'Password reset has expired.'
+      redirect_to new_password_reset_url
     end
   end
 end
