81d9056da42c05ad6108ac0137ae7036455577a6
sudame <su.glasses.923@gmail.com>
2019-01-13 22:13:14 +0900

    add: password reset edit/update controller

--- rails-tutorial-sample-app/app/controllers/password_resets_controller.rb
+++ rails-tutorial-sample-app/app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  
   def new
   end
 
@@ -14,6 +17,21 @@ class PasswordResetsController < ApplicationController
     render 'new'
   end
 
+  def update
+  end
+
   def edit
+  end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    unless @user&.activated? && @user.authenticated?(:reset, params[:id])
+      redirect_to root_url
+    end
   end
 end
