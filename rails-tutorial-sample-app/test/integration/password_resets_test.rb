88e38d641b17d762ccc1760f25aa0b1a0f008b9f
sudame <su.glasses.923@gmail.com>
2019-01-13 22:57:23 +0900

    update: set nil to reset_digest when user success reset password

--- rails-tutorial-sample-app/test/integration/password_resets_test.rb
+++ rails-tutorial-sample-app/test/integration/password_resets_test.rb
@@ -38,6 +38,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     assert_not flash.empty?
     assert_redirected_to user
+    assert_nil user.reload.reset_digest
   end
 
   test 'expired token' do
