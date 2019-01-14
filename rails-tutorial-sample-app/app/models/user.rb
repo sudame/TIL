c427f2b7c9ac342a2c31f33ddd3ff31a51aa5b0a
sudame <su.glasses.923@gmail.com>
2019-01-14 22:27:51 +0900

    add: active_relationships methods

--- rails-tutorial-sample-app/app/models/user.rb
+++ rails-tutorial-sample-app/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'Relationship',
            foreign_key: 'follower_id',
            dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
@@ -66,6 +67,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where('user_id = ?', id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include? other_user
   end
 
   private
