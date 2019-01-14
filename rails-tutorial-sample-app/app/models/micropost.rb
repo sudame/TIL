a73ff4282aa39f8452438e57df1465261dee64ef
sudame <su.glasses.923@gmail.com>
2019-01-14 19:28:49 +0900

    add: limit for uploaded picture size

--- rails-tutorial-sample-app/app/models/micropost.rb
+++ rails-tutorial-sample-app/app/models/micropost.rb
@@ -1,8 +1,15 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> {order(created_at: :desc)}
 
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, length: {maximum: 140}, presence: true
+  validate :picture_size
+
+  private
+
+  def picture_size
+    errors.add(:picture, 'should be less than 5MB') if picture.size > 5.megabytes
+  end
 end
