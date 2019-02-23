730b8a96aae884d6cd433d3895f80640272998db
sudame <su.glasses.923@gmail.com>
2019-02-24 00:31:06 +0900

    add: get access_token

--- pure-noode-oauth/index.js
+++ pure-noode-oauth/index.js
@@ -1,12 +1,18 @@
 const http = require('http');
+const https = require('https');
 const queryString = require('querystring');
+const url = require('url');
+
 const keys = require('./keys.json');
 
 const CLIENT_ID = keys.clientID;
 const CLIENT_SECRET = keys.clientSecret;
 
 const server = http.createServer((req, res) => {
-  switch (req.url) {
+  const reqURL = url.parse(req.url);
+  const reqPathName = reqURL.pathname;
+
+  switch (reqPathName) {
     case '/':
       const requrestBody = queryString.stringify({
         client_id: CLIENT_ID,
@@ -14,9 +20,37 @@ const server = http.createServer((req, res) => {
       });
       res.writeHead(303, { 'Location': `https://github.com/login/oauth/authorize?${requrestBody}` });
       res.end();
-    case '/hoge':
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('hoge page');
+    case '/callback':
+
+      console.log(queryString.parse(reqURL.query).code);
+      const requestBody = queryString.stringify({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code: queryString.parse(reqURL.query).code
+      });
+
+
+      const requestClient = https.request('https://github.com/login/oauth/access_token', {
+        method: 'POST',
+        hostname: 'github.com',
+        port: 443,
+        path: '/login/oauth/access_token',
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+          'Content-Length': requestBody.length
+        },
+      }, (responseClient) => {
+        console.log(responseClient.statusCode);
+        responseClient.setEncoding('utf8');
+        responseClient.on('data', (d) => {
+          console.log(queryString.parse(d));
+        })
+      });
+
+      requestClient.write(requestBody);
+
+      res.writeHead(200);
+      res.end();
     default:
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end();
