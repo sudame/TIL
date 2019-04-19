79d92dab5e829bcd9e7c13991695608118547576
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-04-19 13:54:49 +0900

    add: models and bloc

---
+++ flutter/sudame_todo_bloc/lib/blocs/tasks_bloc.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+
+import 'package:rxdart/subjects.dart';
+import 'package:sudame_todo_bloc/models/task.dart';
+
+class TaskBloc {
+  // State
+  List<Task> _taskList = [];
+  int _editingTask = 0;
+
+  // output controller(s)
+  final BehaviorSubject<List<Task>> _taskListSubject =
+      BehaviorSubject.seeded([]);
+
+  // input controller(s)
+  final StreamController<int> _setEditingController = StreamController();
+  final StreamController<String> _setTitleController = StreamController();
+  final StreamController<String> _setDescriptionController = StreamController();
+  final StreamController<bool> _setIsCompletedController = StreamController();
+
+  // output stream
+  Stream<List<Task>> get getTaskList => _taskListSubject.stream;
+
+  // input stream
+  Sink<int> get setTaskEditing => _setEditingController.sink;
+  Sink<String> get setTaskTitle => _setTitleController.sink;
+  Sink<String> get setTaskDescription => _setDescriptionController.sink;
+  Sink<bool> get setTaskIsCompleted => _setIsCompletedController.sink;
+
+  void _setEditing(int taskId) {
+    // ToDo: implements method.
+  }
+
+  void _setTitle(String title) {
+    // ToDo: implements method.
+  }
+
+  void _setDescription(String description) {
+    // ToDo: implements method.
+  }
+
+  void _setIsCompleted(bool isCompleted) {
+    // ToDo: implements method.
+  }
+
+  TaskBloc() {
+    _setEditingController.stream.listen((int taskId) {
+      _setEditing(taskId);
+    });
+
+    _setTitleController.stream.listen((String title) {
+      _setTitle(title);
+    });
+
+    _setDescriptionController.stream.listen((String description) {
+      _setDescription(description);
+    });
+
+    _setIsCompletedController.stream.listen((bool isCompleted) {
+      _setIsCompleted(isCompleted);
+    });
+  }
+}
---
+++ flutter/sudame_todo_bloc/lib/models/task.dart
@@ -0,0 +1,13 @@
+class Task {
+  final int id;
+  final String title;
+  final String description;
+  final bool isCompleted;
+
+  Task({
+    this.id,
+    this.title,
+    this.description,
+    this.isCompleted = false,
+  });
+}
